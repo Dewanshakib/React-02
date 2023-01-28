35d1ee3c2319c399a3403d117ce1933df102698b
Dewan <shakibrocks2042@gmail.com>
2023-01-29 01:10:04 +0600

    first commit

---
+++ src/components/Image.jsx
@@ -0,0 +1,9 @@
+import "./Image.css"
+
+export const Image = () => {
+    return (
+        <div className="User-img">
+            <img src="./src/assets/User.png" alt="User image" className="User_img"/>
+        </div>
+    )
+}
